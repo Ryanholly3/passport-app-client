0f466e86cef21ed5629693e791b1fb073a7a4c80
chris <verzetem@gmail.com>
2018-11-26 15:04:20 -0700

    added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginSignupComponent } from './login-signup/login-signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginSignupComponent
   ],
   imports: [
     BrowserModule,

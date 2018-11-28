3c275a61c90a0cb0206e823593fcaa67995105b6
chris <verzetem@gmail.com>
2018-11-27 22:46:07 -0700

    added profile information using json data within fakeUser.json. overwrote bootstrap class/added opacity to navbar and profile card to match login-signin menu styles

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import fakeUser from '../fakeUser.json';
 
 @Component({
   selector: 'app-profile',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+	users = fakeUser
+	title = 'My Profile'
 
   constructor() { }
 

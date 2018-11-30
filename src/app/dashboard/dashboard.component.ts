698ee7e70676639989b5cae5c7150539e6260933
ryanholly3 <ryanholly3@gmail.com>
2018-11-29 18:08:04 -0700

    can drop pin on map

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,6 +12,10 @@ export class DashboardComponent implements OnInit {
   latitude= 51.678418;
   longitude= 7.809007;
 
+  latitudeClicked;
+  longitudeClicked;
+  locationChosen=false;
+
   user= [];
   userId;
   visits= [];
@@ -46,5 +50,11 @@ export class DashboardComponent implements OnInit {
     })
     this.filteredDestinations = filtered
     console.log(this.filteredDestinations)
+  }
+
+  chooseLocation(event){
+    this.latitudeClicked = event.coords.lat;
+    this.longitudeClicked = event.coords.lng;
+    this.locationChosen=true;
   }
 }

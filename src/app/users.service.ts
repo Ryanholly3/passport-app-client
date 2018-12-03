a170d86bf367372bf691d5e8021fc3faa17a4050
ryanholly3 <ryanholly3@gmail.com>
2018-12-03 14:29:25 -0700

    added backend deployedDB/localhost flexibility

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -11,27 +11,64 @@ export class UsersService {
   user = []
   users = []
   usersDestinations = []
+  filteredDestinations = []
+
+  backEnd;
 
   constructor() { }
 
-  fetchUser(selectedUser) {
-     fetch(`http://localhost:3100/passport_users/${selectedUser}`)
-      .then(response => response.json())
-      .then(json => {
-        this.user = json.user
+  checkBackendConnection() {
+    fetch(`http://localhost:3100/`)
+     .then(response => {
+         this.backEnd = 'localhost'
+         this.fetchAllUsers()
+         this.fetchDestinations()
+      })
+      .catch(response => {
+        this.backEnd = 'deployedBackend'
+        this.fetchAllUsers()
+        this.fetchDestinations()
       })
   }
 
+  fetchUser(selectedUser) {
+    if(this.backEnd === "localhost"){
+      fetch(`http://localhost:3100/passport_users/${selectedUser}`)
+       .then(response => response.json())
+       .then(json => {
+         this.user = json.user
+       })
+    } if (this.backEnd === "deployedBackend"){
+      fetch(`https://evening-refuge-33727.herokuapp.com/passport_users/${selectedUser}`)
+       .then(response => response.json())
+       .then(json => {
+         this.user = json.user
+       })
+    }
+  }
+
+
   fetchAllUsers() {
-    fetch(`http://localhost:3100/passport_users`)
-     .then(response => response.json())
-     .then(json => {
-       this.users = json.users
-     })
+    if(this.backEnd === "localhost"){
+      fetch(`http://localhost:3100/passport_users`)
+       .then(response => response.json())
+       .then(json => {
+         this.users = json.users
+       })
+    } if(this.backEnd === "deployedBackend"){
+      fetch(`https://evening-refuge-33727.herokuapp.com/passport_users`)
+       .then(response => response.json())
+       .then(json => {
+         this.users = json.users
+       })
+    }
   }
 
   removeUser(){
     this.user = []
+    this.users = []
+    this.usersDestinations = []
+    this.filteredDestinations = []
   }
 
   getUser(){
@@ -39,15 +76,31 @@ export class UsersService {
   }
 
   fetchDestinations(){
-    fetch(`http://localhost:3100/destinations`)
-     .then(response => response.json())
-     .then(dest => {
-       this.usersDestinations = dest.destinations
-     })
+
+    if(this.backEnd === "localhost"){
+      fetch(`http://localhost:3100/destinations`)
+       .then(response => response.json())
+       .then(dest => {
+         this.usersDestinations = dest.destinations
+       })
+    } if (this.backEnd === "deployedBackend"){
+      fetch(`https://evening-refuge-33727.herokuapp.com/destinations`)
+       .then(response => response.json())
+       .then(dest => {
+         this.usersDestinations = dest.destinations
+       })
+    }
   }
 
-  getDestinations(){
-    return this.usersDestinations
+  filterDestinations(selectedUser){
+    const filtered = this.usersDestinations.filter((dest)=>{
+      return dest.passport_users_id !== parseInt(selectedUser)
+    })
+    this.filteredDestinations = filtered
+  }
+
+  getFilteredDestinations(){
+    return this.filteredDestinations
   }
 
 }
